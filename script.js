9418a3cd6cd05c407d309c45103be26ae445f01c
DheeruYS <sudheerays@gmail.com>
2024-02-04 17:00:03 +0530

    Final

--- script.js
+++ script.js
@@ -1,9 +1,69 @@
-var cur = 0;
+function setTheme(){
+    if(localStorage.getItem("theme") == null) localStorage.setItem("theme",0);
+    loadTheme();
+}
 
 function changeTheme(){
-    if(cur == 0){
+    localStorage.setItem("theme",localStorage.getItem("theme")^1);
+    loadTheme();
+}
 
-        document.getElementById("changetheme").innerHTML = "Light Mode"; 
+function setData(idd){
+    let x = localStorage.getItem(idd);
+    if(x == null) x = 0;
+    document.getElementById("number").innerHTML = x;
+    localStorage.setItem(idd, x);
+    if(x > 0) document.getElementsByClassName('fa-heart')[0].style.color='red';
+
+    let y = localStorage.getItem(idd+"com");
+    if(y == null || y == "") localStorage.setItem(idd+"com","");
+    else{
+        let comojb = JSON.parse(y);
+        for (var i in comojb) printComment(comojb[i],idd);
+    }
+
+    setTheme();
+}
+
+function counter(idd){
+    document.getElementById("number").innerHTML = 1;
+    localStorage.setItem(idd, 1);
+    document.getElementsByClassName('fa-heart')[0].style.color='red';
+}
+
+function addComment(comment, idd){
+    if(comment == "") return;
+
+    let y = localStorage.getItem(idd+"com");
+    comArray = [];
+    if(y != null && y != ""){
+        comobj = JSON.parse(y);
+        for(var i in comobj) comArray.push(comobj[i]);
+    }
+    comArray.push(comment);
+
+    let newstring = JSON.stringify(comArray);
+    localStorage.setItem(idd+"com", newstring);
+
+    printComment(comment,idd);
+}
+
+function printComment(comment, idd){
+    if(comment == "") return;
+
+    var v = document.createElement('div');
+    var text = document.createTextNode(comment);
+    v.appendChild(text);
+    document.getElementById('comment1'+idd).appendChild(v);
+}
+
+function loadTheme(){
+    let x = Number(localStorage.getItem("theme"));
+
+    if(x == 1){
+
+        document.getElementById("changetheme").classList.remove("fa-moon-o");
+        document.getElementById("changetheme").classList.add("fa-sun-o");
 
         let cur = document.getElementsByClassName('body');
         for(i = 0; i < cur.length; i++) {
@@ -14,11 +74,6 @@ function changeTheme(){
         cur = document.getElementsByClassName('container1');
         for(i = 0; i < cur.length; i++) cur[i].style.border = '3px solid white';
         
-
-        // cur = document.getElementsByClassName('container');
-        // for(i = 0; i < cur.length; i++) cur[i].style.bordercolor = 'white ';
-        
-
         cur = document.getElementsByClassName('fa-github');
         for(i = 0; i < cur.length; i++) cur[i].style.color = 'white';
         
@@ -30,7 +85,8 @@ function changeTheme(){
         for(i = 0; i < cur.length; i++) cur[i].style.color = 'black';
     }
     else{
-        document.getElementById("changetheme").innerHTML = "Dark Mode"; 
+        document.getElementById("changetheme").classList.remove("fa-sun-o");
+        document.getElementById("changetheme").classList.add("fa-moon-o");
 
         let cur = document.getElementsByClassName('body');
         for(i = 0; i < cur.length; i++) {
@@ -40,11 +96,6 @@ function changeTheme(){
 
         cur = document.getElementsByClassName('container1');
         for(i = 0; i < cur.length; i++) cur[i].style.border = '3px solid black';
-        
-
-        // cur = document.getElementsByClassName('container');
-        // for(i = 0; i < cur.length; i++) cur[i].style.border.color = 'black ';
-        
 
         cur = document.getElementsByClassName('fa-github');
         for(i = 0; i < cur.length; i++) cur[i].style.color = 'black';
@@ -57,6 +108,4 @@ function changeTheme(){
         cur = document.getElementsByClassName('navlink');
         for(i = 0; i < cur.length; i++) cur[i].style.color = 'white';
     }
-
-    cur = cur^1;
 }
